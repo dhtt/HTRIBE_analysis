6456791ce1d057a4397d406885bc4b85da08fe12
dhthutrang <dhthutrang@gmail.com>
2024-10-27 15:35:40 +0100

    Modify homer motif analysis

--- code/run_homer.sh
+++ code/run_homer.sh
@@ -1,3 +1,5 @@
+background_path=/home/dhthutrang/TRIBE/mRNA_seq/processed/extract.trim.align.dedup/bed
+
 for file in expand_site_nomerged/*/*.bed;
 do (
     FILENAME=${file%%.*}
@@ -19,12 +21,15 @@ do (
     if [ $threshold = '_A2G' ]; then
         threshold=''
     fi
-    mkdir -p $FILEPATH/findMotifsGenome $FILEPATH/findMotifsGenome_bg $FILEPATH/findMotifs $FILEPATH/findMotifs_bg
+    mkdir -p $FILEPATH/findMotifsGenome $FILEPATH/findMotifsGenome_bg $FILEPATH/findMotifsGenome_bg_raw $FILEPATH/findMotifs $FILEPATH/findMotifs_bg $FILEPATH/findMotifs_bg_raw
     
-    findMotifsGenome.pl $file mm39 $FILEPATH/findMotifsGenome/$FILENAME/ -bits -len 5
-    findMotifsGenome.pl $file mm39 $FILEPATH/findMotifsGenome_bg/$FILENAME/ -bits -len 5 -bg $FILEPATH/wt_mcherry_A2G_$threshold%.bed
-    findMotifs.pl gene_list/$FILENAME.txt mouse $FILEPATH/findMotifs/$FILENAME -bits -len 5
-    echo "findMotifs.pl $FILEPATH/gene_list/$FILENAME.txt mouse $FILEPATH/findMotifs_bg/$FILENAME -bits -len 5 -bg $FILEPATH/gene_list/wt_mcherry_A2G$threshold.txt"
+    findMotifsGenome.pl $file mm39 $FILEPATH/findMotifsGenome/$FILENAME/ -bits -len 5 -rna
+    findMotifsGenome.pl $file mm39 $FILEPATH/findMotifsGenome_bg/$FILENAME/ -bits -len 5 -bg $FILEPATH/wt_mcherry_A2G$threshold.bed -rna
+    findMotifsGenome.pl $file mm39 $FILEPATH/findMotifsGenome_bg_raw/$FILENAME/ -bits -len 5 -bg $background_path/control.bed -rna
+
+    findMotifs.pl $FILEPATH/gene_list/$FILENAME.txt mouse $FILEPATH/findMotifs/$FILENAME -bits -len 5 -rna
+    findMotifs.pl $FILEPATH/gene_list/$FILENAME.txt mouse $FILEPATH/findMotifs_bg/$FILENAME -bits -len 5 -bg $FILEPATH/gene_list/wt_mcherry_A2G$threshold.txt -rna
     ) &
 done    
-
+wait
+echo "FINISH"
